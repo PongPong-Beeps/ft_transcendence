d726f773d4eb4960339d4e6d91978106ef0c1ec1
noeyiz <lamoureclatant@gmail.com>
2024-02-20 17:43:47 +0900

    :sparkles: feat: 프로필 화면 구현

--- srcs/FrontEnd/srcs/components/my-profile.js
+++ srcs/FrontEnd/srcs/components/my-profile.js
@@ -4,7 +4,12 @@
 export default function MyProfile($container) {
     const render = () => {
         $container.querySelector('#profile').innerHTML = `
-            <div>MyProfile</div>
+            <link rel="stylesheet" href="../../assets/css/profile.css">
+            <div id="profile-container">
+                <div id="profile-image"></div>
+                <div id="nickname">닉네임</div>
+                <button class="green-btn" id="detail-btn">상세 정보</button>
+            </div>
         `;
     }
 
