f25e29d243ce40881869578c807282e8e372e43d
cocomhwa <swk951031@gmail.com>
2024-02-23 19:37:14 +0900

    :sparkles: feat: 대전 상대 표시 페이지 이미지 추가 및 틀 구현

---
+++ srcs/FrontEnd/srcs/pages/vs-schedule.js
@@ -0,0 +1,16 @@
+/**
+ * @param {HTMLElement} $container
+ */
+export default function VsSchedule($container) {
+    const render = () => {
+        $container.querySelector('#page').innerHTML = `
+            <link rel="stylesheet" href="../../assets/css/vs-schedule.css">
+            <div id="VsSchedule" class="vs-screen">
+                <img src="assets/image/cardFrame.jpg" alt="나" class="player player1">
+                <div class="vs-text">vs</div>
+                <img src="assets/image/cardFrame.jpg" alt="상대" class="player player2">
+            </div>
+        `;
+    };
+    render();
+}
