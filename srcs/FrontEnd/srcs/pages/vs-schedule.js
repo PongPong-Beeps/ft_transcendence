03ac3cf50470a587d8b9ed76311a90c0e7cbc68a
cocomhwa <swk951031@gmail.com>
2024-02-25 19:03:21 +0900

    :bug: fix: vs-schedule화면 배경에 레이아웃 나오는 문제 해결 :green_heart: chore: 한글 사용 부분 영문으로 변경, 글자 크기 변경

--- srcs/FrontEnd/srcs/pages/vs-schedule.js
+++ srcs/FrontEnd/srcs/pages/vs-schedule.js
@@ -6,9 +6,9 @@ export default function VsSchedule($container) {
         $container.querySelector('#page').innerHTML = `
             <link rel="stylesheet" href="../../assets/css/vs-schedule.css">
             <div id="VsSchedule" class="vs-screen">
-                <img src="assets/image/cardFrame.jpg" alt="나" class="player player1">
+                <img src="assets/image/cardFrame.jpg" alt="me" class="player player1">
                 <div class="vs-text">vs</div>
-                <img src="assets/image/cardFrame.jpg" alt="상대" class="player player2">
+                <img src="assets/image/cardFrame.jpg" alt="you" class="player player2">
             </div>
         `;
     };
