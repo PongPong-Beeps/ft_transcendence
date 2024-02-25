ada183464ddd096618780f444979b66b05da129e
cocomhwa <swk951031@gmail.com>
2024-02-25 22:10:52 +0900

    :sparkles: feat: 대전상대 카드를 동적으로 생성하고, 대전 상대의 정보를 표시 :art: style: 카드함수 파일을 분리하고, 카드함수를 임포트하여 사용

---
+++ srcs/FrontEnd/srcs/pages/vs-schedule-user-card.js
@@ -0,0 +1,10 @@
+export default function VsScheduleUserCard(name, profileImg) {
+  return `
+        <div id="VsScheduleUserCard" class="card">
+            <div class="profile-image">
+                <img src="${profileImg[0].image}" alt="${profileImg[0].label}"/>
+            </div>
+            <div class="user-name">${name}</div>
+        </div>
+  `;
+}
--- srcs/FrontEnd/srcs/pages/vs-schedule.js
+++ srcs/FrontEnd/srcs/pages/vs-schedule.js
@@ -1,16 +1,22 @@
 /**
  * @param {HTMLElement} $container
  */
+
+import VsScheduleUserCard from "./vs-schedule-user-card.js";
+
 export default function VsSchedule($container) {
+    let profileImg = [
+        { label: 'profile-image-file', image: '../../../assets/image/pongLogo.png' },
+    ];
     const render = () => {
-        $container.querySelector('#page').innerHTML = `
+        $container.querySelector("#page").innerHTML = `
             <link rel="stylesheet" href="../../assets/css/vs-schedule.css">
             <div id="VsSchedule" class="vs-screen">
-                <img src="assets/image/cardFrame.jpg" alt="me" class="player player1">
+                ${VsScheduleUserCard("me", profileImg)}
                 <div class="vs-text">vs</div>
-                <img src="assets/image/cardFrame.jpg" alt="you" class="player player2">
+                ${VsScheduleUserCard("you", profileImg)}
             </div>
         `;
-    };
-    render();
+  };
+  render();
 }
