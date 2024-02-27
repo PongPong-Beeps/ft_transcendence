8b43947349b7c2666874f96e4f61822173120f42
최종원 <jonnwon@choejong-won-ui-MacBookAir-2.local>
2024-02-27 16:54:11 +0900

    ✨ :sparkles: feat: login 후 callback url패턴 및 JsonResponse 기반 구축

--- srcs/BackEnd/project/login/urls.py
+++ srcs/BackEnd/project/login/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import login42TestView
+from .views import login42TestView, login42CallbackView
 
 urlpatterns = [
     path('42/', login42TestView.as_view(), name='42login'),
+    path('42/callback/', login42CallbackView.as_view(), name='42callback'),
 ]
--- srcs/BackEnd/project/login/views.py
+++ srcs/BackEnd/project/login/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import redirect
 from django.views import View
+from django.http import HttpResponse, JsonResponse
+
 
 class login42TestView(View):
     def get(self, request):  # 수정: get 메서드의 첫 번째 매개변수는 self여야 합니다.
-        target_url = 'https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-6da803718d6fd73157034c6b420e69e7edc0e341297bc27bbccd7c840bfbbdd4&redirect_uri=https%3A%2F%2F127.0.0.1%2Flobby&response_type=code'
+        target_url = 'https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-942fd4b0016fa5993b2a57b000789a0d9b6e6b05ef6a004724062905ea9dc440&redirect_uri=https%3A%2F%2F127.0.0.1%2Fauth&response_type=code'
         return redirect(target_url)  # 수정: return 문에 괄호를 제거합니다.
+
+
+class login42CallbackView(View):
+    def post(self, request):
+        print("POST 요청이 수신되었습니다.")
+        return JsonResponse({'message': 'This is a successful response.'}, status=200)
