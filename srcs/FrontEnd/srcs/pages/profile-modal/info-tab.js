1e2a8d4851f94d7957d075af18a2e223656c56b4
cocomhwa <swk951031@gmail.com>
2024-02-28 22:16:10 +0900

    :green_heart: chore: vs.png 변경 :sparkles: feat: 프로필 모달 정보 탭 진행 중

--- srcs/FrontEnd/srcs/pages/profile-modal/info-tab.js
+++ srcs/FrontEnd/srcs/pages/profile-modal/info-tab.js
@@ -1,48 +1,49 @@
-// export default function InfoTab(nickname, isMe, totalWinRate, oneOnOneWinRate, tournamentWinRate) {
-//     return `
-//         <div id="info-tab-container">
-//             <div id="profile-picture-container">
-//                 <img src="../../../assets/image/tournament.png" alt="profile picture">
-//                 ${isMe ? '<button id="change-picture-button">Change Picture</button>' : ''}
-//             </div>
-//             <div id="profile-info-container">
-//                 <div id="nickname-container">${nickname}</div>
-//                 <div id="total-win-rate-container">전체 승률: ${totalWinRate}%</div>
-//                 <div id="one-on-one-win-rate-container">1대1 승률: ${oneOnOneWinRate}%</div>
-//                 <div id="tournament-win-rate-container">토너먼트 승률: ${tournamentWinRate}%</div>
-//             </div>
-//         </div>
-//     `;
-// }
-
 export default function InfoTab(nickname, isMe, totalWinRate, oneOnOneWinRate, tournamentWinRate) {
-    const totalWinRateBar = createWinRateBar(totalWinRate);
-    const oneOnOneWinRateBar = createWinRateBar(oneOnOneWinRate);
-    const tournamentWinRateBar = createWinRateBar(tournamentWinRate);
-
+    const totalWinRateBar = createWinRateBar(totalWinRate, '전체');
+    const oneOnOneWinRateBar = createWinRateBar(oneOnOneWinRate, '1vs1');
+    const tournamentWinRateBar = createWinRateBar(tournamentWinRate, '토너먼트');
+    let nicknameBox = createNicknameBox(nickname, isMe);
     return `
-        <div id="info-tab-container">
-            <div id="profile-picture-container">
-                <img src="../../../assets/image/cruiser.gif" alt="profile picture">
-                ${isMe ? '<button id="change-picture-button">Change Picture</button>' : ''}
-            </div>
-            <div id="profile-info-container">
-                <div id="nickname-container">${nickname}</div>
-                <div id="total-win-rate-container">
-                    전체 승률: ${totalWinRate}% ${totalWinRateBar}
-                </div>
-                <div id="one-on-one-win-rate-container">
-                    1대1 승률: ${oneOnOneWinRate}% ${oneOnOneWinRateBar}
-                </div>
-                <div id="tournament-win-rate-container">
-                    토너먼트 승률: ${tournamentWinRate}% ${tournamentWinRateBar}
-                </div>
+        <div id="profile-picture-container">
+            <img src="../../../assets/image/cruiser.gif" alt="profile picture">
+            <div id="change-picture-container">
+                ${isMe ? '<button id="change-picture-button">*</button>' : ''}
             </div>
         </div>
-    `;
+        <div id="nickname-container">
+            ${nicknameBox}
+        </div>
+        <div id="winrate-container">
+            <div id="total-win-rate-container">
+                ${totalWinRateBar}
+            </div>
+            <div id="one-on-one-win-rate-container">
+                ${oneOnOneWinRateBar}
+            </div>
+            <div id="tournament-win-rate-container">
+                ${tournamentWinRateBar}
+            </div>
+        </div>
+     `;
 }
 
-function createWinRateBar(winRate) {
+function createWinRateBar(winRate, type) {
     const barWidth = winRate + '%';
-    return `<div class="win-rate-bar" style="width: ${barWidth};"></div>`;
+    const gaugeColor = winRate >= 50 ? 'steelblue' : 'coral';
+    return `
+        <div class="win-rate-box">
+            <div class="type-box">${type}</div>
+            <div class="win-rate-gauge-box">
+                <div class="win-rate-gauge" style="width: ${barWidth};background-color: ${gaugeColor}; border-radius: 5px; overflow: hidden;"></div>
+            </div>
+        </div>`;
+}
+
+function createNicknameBox(nickname, isMe = true) {
+    return `
+        <div id="nickname-Box">
+            ${nickname}
+        </div>
+        ${isMe ? '<button id="change-nickname-btn">변경</button>' : ''}
+    `;
 }
