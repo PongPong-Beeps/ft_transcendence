cdc2de3d216f0048459eb19031ee332785e76a9c
noeyiz <lamoureclatant@gmail.com>
2024-02-28 16:46:14 +0900

    :bulb: comment: 테스트 주석 추가

--- srcs/FrontEnd/srcs/components/user-list/user-list.js
+++ srcs/FrontEnd/srcs/components/user-list/user-list.js
@@ -33,8 +33,7 @@ export default function UserList($container) {
                 <div id="user-list-list-container">
                     <div id="friends-list" class="list"></div>
                     <div id="all-list" class="list" style="display: none;">
-<!--                        <div class="user">사용자 1</div>-->
-<!--                        <div class="user">사용자 2</div>-->
+                        <!-- 테스트용 -->
                         <button id="four_zero_one">401</button>
                         <button id="four_zero_four">404</button>
                         <button id="five_zero_zero">500</button>
@@ -89,11 +88,9 @@ export default function UserList($container) {
         $container.querySelector('#four_zero_one').addEventListener('click', () => {
             new Error($container, 401);
         });
-
         $container.querySelector('#four_zero_four').addEventListener('click', () => {
             new Error($container, 404);
         });
-
         $container.querySelector('#five_zero_zero').addEventListener('click', () => {
             new Error($container, 500);
         });
