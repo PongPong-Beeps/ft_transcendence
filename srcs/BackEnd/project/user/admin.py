1d1bc446b5516cc9813de3a0660a86c23b1ddd13
jonnwon <jonnweon@gmail.com>
2024-03-02 23:56:14 +0900

    :green_heart: chore: 불필요한 admin사이트 등록 코드 삭제

--- srcs/BackEnd/project/user/admin.py
+++ srcs/BackEnd/project/user/admin.py
@@ -2,8 +2,3 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(User)
-admin.site.register(Friend)
-admin.site.register(GameRecord)
-admin.site.register(Blacklist)
-admin.site.register(UserList)
-admin.site.register(GameRoom)
