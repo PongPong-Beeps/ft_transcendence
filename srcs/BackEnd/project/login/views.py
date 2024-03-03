45884f211ccb98227aca1352e8d3104101f3e470
jonnwon <jonnweon@gmail.com>
2024-03-04 05:17:12 +0900

    :sparkles: feat: Customizing token claims 설정 및 set_cookie 시 토큰 분리

--- srcs/BackEnd/project/login/views.py
+++ srcs/BackEnd/project/login/views.py
@@ -6,9 +6,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 import datetime
-from django.conf import settings
-from rest_framework_simplejwt.tokens import RefreshToken
 from user.models import User
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
 
 grant_type = 'authorization_code'
 client_id = 'u-s4t2ud-942fd4b0016fa5993b2a57b000789a0d9b6e6b05ef6a004724062905ea9dc440'
@@ -71,21 +71,34 @@ class Login42CallbackView(APIView):
 
             # Response 객체 생성
             response = Response({"message": "POST 요청 처리 완료"}, status=status.HTTP_200_OK)
+            
+            # access_token 쿠키 설정
             response.set_cookie(
-                key='jwt_token',
-                value=jwt_token,
-                httponly=False,  # JavaScript를 통한 접근 방지(True 라면)
+                key='access_token',
+                value=jwt_token['access_token'],
+                httponly=False,  # JavaScript를 통한 접근 방지 해제
                 expires=datetime.datetime.utcnow() + datetime.timedelta(days=1),  # 쿠키 만료 시간 설정
                 secure=True,  # HTTPS를 통해서만 쿠키 전송
                 samesite='Lax'  # CSRF 보호를 위한 설정
             )
+
+            # refresh_token 쿠키 설정
+            response.set_cookie(
+                key='refresh_token',
+                value=jwt_token['refresh_token'],
+                httponly=False,  # JavaScript를 통한 접근 방지 해제 
+                expires=datetime.datetime.utcnow() + datetime.timedelta(days=1),  # 쿠키 만료 시간 설정
+                secure=True,  # HTTPS를 통해서만 쿠키 전송
+                samesite='Lax'  # CSRF 보호를 위한 설정
+                )
             return response
         else:
             return Response({"message": "POST 요청 실패"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 def create_jwt_token(user):
-    refresh = RefreshToken.for_user(user)
+    refresh = MyTokenObtainPairSerializer.get_token(user)
+
     return {
         'access_token': str(refresh.access_token),
         'refresh_token': str(refresh),
@@ -104,3 +117,13 @@ def save_user_to_db(_user_id, _user_email, _user_nickname):
         print(_user_id, _user_email, " saved to db")
     else :
         print(_user_id, _user_email, " aleady saved db")
+        
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # 사용자의 nickname을 토큰에 추가
+        token['nickname'] = user.nickname
+
+        return token
