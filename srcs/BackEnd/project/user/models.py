4daeec3bbd414783409ae8e55e3dd3211f722f41
jonnwon <bssong35@gmail.com>
2024-03-05 17:18:11 +0900

    :green_heart: chore: user model 내 is_online 삭제

--- srcs/BackEnd/project/user/models.py
+++ srcs/BackEnd/project/user/models.py
@@ -3,7 +3,6 @@ from django.db import models
 class User(models.Model) :
     nickname = models.CharField(max_length=20)
     email = models.EmailField(unique=True)
-    # is_online = models.BooleanField(default=False)
     # login_type = models.CharField(max_length=50)
 
     # REQUIRED_FIELDS = ['email']  # 사용자 생성 시 필수로 입력해야 하는 필드
