77d358a6a0d9796ecc7463333db4b7bad6a8bffa
jonnwon <bssong35@gmail.com>
2024-03-05 17:43:09 +0900

    :sparkles: feat: api/user/me 엔드포인트 구분

--- srcs/BackEnd/project/user/views.py
+++ srcs/BackEnd/project/user/views.py
@@ -1,12 +1,31 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .models import User  # 가정한 모델 임포트 경로
+from .models import User
+from rest_framework.permissions import IsAuthenticated
 
+
+#/api/user/list
 class UserListView(APIView):
+#    permission_classes = [IsAuthenticated]
+
     def get(self, request):
         users = User.objects.all()
         # 각 사용자의 닉네임을 'nickname' 키를 가진 딕셔너리로 변환
         user_list = [{"nickname": user.nickname} for user in users]
         # 'userList' 키 아래에 사용자 리스트를 포함하는 JSON 객체로 응답 구성
         response_data = {"userList": user_list}
+        return Response(response_data)
+
+#/api/user/me
+class CurrentUserView(APIView):
+    # permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user_id = request.user.id
+        user = User.objects.get(id=user_id)
+        print(user)
+        response_data = {
+                 "nickname": user.nickname,
+                 #추후 이미지도 추가해야함
+        }
         return Response(response_data)
