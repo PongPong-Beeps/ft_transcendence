60313236586c939d6b390ef165e31e58b797a862
noeyiz <lamoureclatant@gmail.com>
2024-03-06 16:40:06 +0900

    :sparkles: feat: 필요없는 변수 삭제 후 로직 변경

--- srcs/FrontEnd/srcs/pages/Error.js
+++ srcs/FrontEnd/srcs/pages/Error.js
@@ -6,25 +6,6 @@ import {navigate} from "../utils/navigate.js";
  * @param { number } errorCode
  */
 export default function Error($container, errorCode = 0) {
-    let errorMessage = ""
-
-    const initErrorMessage = () => {
-        switch (errorCode) {
-            case 400:
-                errorMessage = "잘못된 요청입니다";
-                break;
-            case 401:
-                errorMessage = "권한이 없습니다";
-                break;
-            case 500:
-                errorMessage = "서버 오류 (ㅠ ㅠ)";
-                break;
-            default:
-                errorMessage = "알 수 없는 오류입니다";
-                break;
-        }
-    }
-
     const render = () => {
         const page = $container.querySelector('#page');
         if (page) {
@@ -33,21 +14,34 @@ export default function Error($container, errorCode = 0) {
                     <div id="home">
                         <img id="error_face" src="../../assets/image/error_face.png"  alt="error_face" />
                     </div>
-                    <div id="error_message">${errorMessage}</div>
+                    <div id="error_message">${getErrorMessage()}</div>
                 </div>
             `;
             page.style.display = 'block';
         }
     }
 
+    const getErrorMessage = () => {
+        switch (errorCode) {
+            case 400:
+                return "잘못된 요청입니다";
+            case 401:
+                return "권한이 없습니다";
+            case 500:
+                return  "서버 오류 (ㅠ ㅠ)";
+            default:
+                return "알 수 없는 오류입니다";
+        }
+    }
+
     const setupEventListener = () => {
-        $container.querySelector('#home').addEventListener('click', () => {
+        const homeButton = $container.querySelector('#home');
+        homeButton.addEventListener('click', () => {
             navigate('/');
         });
     }
 
     importCss("assets/css/error.css");
-    initErrorMessage();
     render();
     setupEventListener();
 }
