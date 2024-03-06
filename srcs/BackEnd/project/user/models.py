7d12bacc76e682de2d8f663743313a0598663a22
jonnwon <bssong35@gmail.com>
2024-03-06 19:17:26 +0900

    :sparkles: feat: matchhistory 모델 구현

--- srcs/BackEnd/project/user/models.py
+++ srcs/BackEnd/project/user/models.py
@@ -9,3 +9,14 @@ class User(models.Model) :
 
     def __str__(self):
         return self.nickname
+
+
+class MatchHistory(models.Model) :
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='match_history')
+    
+    p1 = models.CharField(max_length=20)    # player1
+    p2 = models.CharField(max_length=20)    # player2
+    datetime = models.DateTimeField(auto_now=False, auto_now_add=True) # 게임 날짜
+    tournament = models.BooleanField(null=True) # true 면 tournament, false 면 normal
+    easy_mode = models.BooleanField(null=True)       # true 면 easy, false 면 hard
+    result = models.BooleanField(null=True)     # true 면 win, false 면 lose
