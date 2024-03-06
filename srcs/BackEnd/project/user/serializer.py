a1ac689b0c5fa24aa786207f3f13214d99e9d248
jonnwon <bssong35@gmail.com>
2024-03-06 20:22:11 +0900

    :green_heart: chore: 불필요한 주석 제거

--- srcs/BackEnd/project/user/serializer.py
+++ srcs/BackEnd/project/user/serializer.py
@@ -3,7 +3,6 @@ from rest_framework import serializers
 
 # Modelserializer : Meta 클래스를 통해 모델과 필드를 연결
 class BlackListSerializer(serializers.ModelSerializer):
-    # user_block = serializers.CharField(max_length=20)
     class Meta:
         model = views.User
         fields = ['nickname']
