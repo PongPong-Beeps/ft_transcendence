c16ea2c07180fb0442b38ebcbf43aa4a95cb1156
geonwule <geonwlee22@gmail.com>
2024-03-06 17:24:55 +0900

    :sparkles: feat: block/unblock/blacklist 기능 추가

--- srcs/BackEnd/project/user/urls.py
+++ srcs/BackEnd/project/user/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('list/', views.UserListView.as_view(), name='user_list'),
+    path('block/', views.BlockUserView.as_view(), name='block'), # 친구 프로필 - 차단
+    path('unblock/', views.UnblockUserView.as_view(), name='unblock'), # 내 프로필 - 블랙리스트 - 차단해제
+    path('blacklist/', views.BlackListView.as_view(), name='blacklist'), # 내 프로필 - 블랙리스트
 ]
