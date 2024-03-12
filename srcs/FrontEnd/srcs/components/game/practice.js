632146ea49df6e3255f72e37e7fe487de3744fcb
noeyiz <lamoureclatant@gmail.com>
2024-03-12 17:20:10 +0900

    :sparkles: 키보드 이벤트 로직 수정 및 게임 시작 전 딜레이

--- srcs/FrontEnd/srcs/components/game/practice.js
+++ srcs/FrontEnd/srcs/components/game/practice.js
@@ -2,17 +2,18 @@ import { importCss } from "../../utils/importCss.js";
 import ExitConfirmation from "../../pages/ExitConfirmation.js";
 
 export default function Practice($container) {
-    let canvas, ctx;
+    let canvas, ctx; // 캔버스 정보
     let player1, player2, ball;
-    let animationFrameId;
-    let isBallMoving = true;
-    const paddleSpeed = 5; // 매 프레임마다 패들이 이동할 거리
+    let animationFrameId; // 게임 루프를 관리하는 데 사용될 id
+    let isBallMoving = false;
     let keys = {
         'w': false,
         's': false,
         'ArrowUp': false,
         'ArrowDown': false
     };
+    const paddleInfo = { width: 12, height: 120, speed: 5, color: 'BLACK' };
+    const ballInfo = { radius: 12, speed: 5, velocityX: 5, velocityY: 5, color: 'RED' };
 
     const gameInit = () => {
         canvas = $container.querySelector('.pong-canvas');
@@ -21,11 +22,14 @@ export default function Practice($container) {
         canvas.width = 800;
         canvas.height = 400;
         // 플레이어, 공 초기화
-        player1 = { x: 20, y: canvas.height / 2 - 60, width: 12, height: 120, color: 'BLACK', score: 0 };
-        player2 = { x: canvas.width - 30, y: canvas.height / 2 - 60, width: 12, height: 120, color: 'BLACK', score: 0 };
-        ball = { x: canvas.width / 2, y: canvas.height / 2, radius: 12, speed: 5, velocityX: 5, velocityY: 5, color: 'RED' };
+        player1 = { x: 20, y: canvas.height / 2 - 60, score: 0 };
+        player2 = { x: canvas.width - 30, y: canvas.height / 2 - 60, score: 0 };
+        ball = { x: canvas.width / 2, y: canvas.height / 2 };
 
         gameLoop();
+        setTimeout(() => {
+            isBallMoving = true;
+        }, 1000);
     };
 
     const gameLoop = () => {
@@ -37,44 +41,43 @@ export default function Practice($container) {
 
     const movePaddle = () => {
         if (keys['w']) {
-            player1.y = Math.max(player1.y - paddleSpeed, 0);
+            player1.y = Math.max(player1.y - paddleInfo.speed, 0);
         }
         if (keys['s']) {
-            player1.y = Math.min(player1.y + paddleSpeed, canvas.height - player1.height);
+            player1.y = Math.min(player1.y + paddleInfo.speed, canvas.height - paddleInfo.height);
         }
         if (keys['ArrowUp']) {
-            player2.y = Math.max(player2.y - paddleSpeed, 0);
+            player2.y = Math.max(player2.y - paddleInfo.speed, 0);
         }
         if (keys['ArrowDown']) {
-            player2.y = Math.min(player2.y + paddleSpeed, canvas.height - player2.height);
+            player2.y = Math.min(player2.y + paddleInfo.speed, canvas.height - paddleInfo.height);
         }
     };
 
     const update = () => {
         if (!isBallMoving) return;
-
         // 공의 위치 업데이트
-        ball.x += ball.velocityX;
-        ball.y += ball.velocityY;
+        ball.x += ballInfo.velocityX;
+        ball.y += ballInfo.velocityY;
         // 벽 충돌 검사
-        if (ball.y + ball.radius > canvas.height || ball.y - ball.radius < 0) {
-            ball.velocityY = -ball.velocityY; // Y 방향 반전
+        if (ball.y + ballInfo.radius > canvas.height || ball.y - ballInfo.radius < 0) {
+            ballInfo.velocityY = -ballInfo.velocityY; // Y 방향 반전
         }
         // 공이 왼쪽 또는 오른쪽 끝에 도달했을 때 점수 처리
-        if (ball.x - ball.radius < 0) { // 왼쪽 벽에 충돌
+        if (ball.x - ballInfo.radius < 0) { // 왼쪽 벽에 충돌
             player2.score++;
             resetBall();
-        } else if (ball.x + ball.radius > canvas.width) { // 오른쪽 벽에 충돌
+        } else if (ball.x + ballInfo.radius > canvas.width) { // 오른쪽 벽에 충돌
             player1.score++;
             resetBall();
         }
         // 패들 충돌 검사
         let nearestPlayer = (ball.x < canvas.width / 2) ? player1 : player2;
-        if (ball.x - ball.radius < nearestPlayer.x + nearestPlayer.width
-            && ball.x + ball.radius > nearestPlayer.x
-            && ball.y - ball.radius < nearestPlayer.y + nearestPlayer.height
-            && ball.y + ball.radius > nearestPlayer.y) {
-            ball.velocityX = -ball.velocityX; // X 방향 반전
+        if (ball.x - ballInfo.radius < nearestPlayer.x + paddleInfo.width
+            && ball.x + ballInfo.radius > nearestPlayer.x
+            && ball.y - ballInfo.radius < nearestPlayer.y + paddleInfo.height
+            && ball.y + ballInfo.radius > nearestPlayer.y) {
+            ballInfo.velocityX = -ballInfo.velocityX; // X 방향 반전
         }
     };
 
@@ -82,10 +85,10 @@ export default function Practice($container) {
         isBallMoving = false;
         ball.x = canvas.width / 2;
         ball.y = canvas.height / 2;
-        ball.speed = 5;
+        ballInfo.speed = 5;
         // 랜덤한 방향으로 시작 각도 조정
-        ball.velocityX = 5 * (Math.random() > 0.5 ? 1 : -1);
-        ball.velocityY = 5 * (Math.random() > 0.5 ? 1 : -1);
+        ballInfo.velocityX = 5 * (Math.random() > 0.5 ? 1 : -1);
+        ballInfo.velocityY = 5 * (Math.random() > 0.5 ? 1 : -1);
         setTimeout(() => {
             isBallMoving = true;
         }, 500);
@@ -95,14 +98,14 @@ export default function Practice($container) {
         // 배경 클리어
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         // 플레이어 패들 그리기
-        ctx.fillStyle = player1.color;
-        ctx.fillRect(player1.x, player1.y, player1.width, player1.height);
-        ctx.fillStyle = player2.color;
-        ctx.fillRect(player2.x, player2.y, player2.width, player2.height);
+        ctx.fillStyle = paddleInfo.color;
+        ctx.fillRect(player1.x, player1.y, paddleInfo.width, paddleInfo.height);
+        ctx.fillStyle = paddleInfo.color;
+        ctx.fillRect(player2.x, player2.y, paddleInfo.width, paddleInfo.height);
         // 공 그리기
         ctx.beginPath();
-        ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI*2, true);
-        ctx.fillStyle = ball.color;
+        ctx.arc(ball.x, ball.y, ballInfo.radius, 0, Math.PI*2, true);
+        ctx.fillStyle = ballInfo.color;
         ctx.fill();
         ctx.closePath();
         // 점수 그리기
@@ -129,25 +132,20 @@ export default function Practice($container) {
         });
 
         document.addEventListener('keydown', (event) => {
-            if (event.key in keys) {
-                keys[event.key] = true;
-            }
-            if (event.code === "KeyW") {
-                keys['w'] = true;
-            } else if (event.code === "KeyS") {
-                keys['s'] = true;
+            switch (event.code) {
+                case 'KeyW': keys['w'] = true; break;
+                case 'KeyS': keys['s'] = true; break;
+                case 'ArrowUp': keys['ArrowUp'] = true; break;
+                case 'ArrowDown': keys['ArrowDown'] = true; break;
             }
         });
 
         document.addEventListener('keyup', (event) => {
-            if (event.key in keys) {
-                keys[event.key] = false;
-            }
-            // 한영키 이슈
-            if (event.code === "KeyW") {
-                keys['w'] = false;
-            } else if (event.code === "KeyS") {
-                keys['s'] = false;
+            switch (event.code) {
+                case 'KeyW': keys['w'] = false; break;
+                case 'KeyS': keys['s'] = false; break;
+                case 'ArrowUp': keys['ArrowUp'] = false; break;
+                case 'ArrowDown': keys['ArrowDown'] = false; break;
             }
         });
     };
