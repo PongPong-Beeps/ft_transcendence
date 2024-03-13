6203cd2e617dc73449e0e6ebad44b946058194d9
noeyiz <lamoureclatant@gmail.com>
2024-03-13 15:09:41 +0900

    :sparkles: 비율로 변경

--- srcs/FrontEnd/srcs/components/game/practice.js
+++ srcs/FrontEnd/srcs/components/game/practice.js
@@ -12,24 +12,30 @@ export default function Practice($container) {
         'ArrowUp': false,
         'ArrowDown': false
     };
-    const paddleInfo = { width: 12, height: 120, speed: 5, color: 'BLACK' };
-    const ballInfo = { radius: 12, speed: 5, velocityX: 5, velocityY: 5, color: 'RED' };
+    const w = window.innerWidth, h = window.innerHeight;
+    console.log(w, h);
+    // easy
+    // const paddle = { width: w * 0.005, height: h * 0.06, speed: w * 0.004, color: 'BLACK' };
+    // const pong = { radius: w * 0.005, speed: w * 0.003, color: 'RED' };
+    // hard
+    const paddle = { width: w * 0.005, height: h * 0.05, speed: w * 0.005, color: 'BLACK' };
+    const pong = { radius: w * 0.005, speed: w * 0.004, color: 'RED' };
 
     const gameInit = () => {
         canvas = $container.querySelector('.pong-canvas');
         ctx = canvas.getContext('2d');
         // 캔버스 크기 설정
-        canvas.width = 800;
-        canvas.height = 400;
+        canvas.width = w * 0.4;
+        canvas.height = h * 0.3;
         // 플레이어, 공 초기화
-        player1 = { x: 0, y: canvas.height / 2 - 60, score: 0 };
-        player2 = { x: canvas.width - 12, y: canvas.height / 2 - 60, score: 0 };
-        ball = { x: canvas.width / 2, y: canvas.height / 2 };
+        player1 = { x: 0, y: canvas.height / 2 - paddle.height / 2, score: 0 };
+        player2 = { x: canvas.width - paddle.width, y: canvas.height / 2 - paddle.height / 2, score: 0 };
+        ball = { x: canvas.width / 2, y: canvas.height / 2, dirX: 1, dirY: 1,};
 
         gameLoop();
         setTimeout(() => {
             isBallMoving = true;
-        }, 1000);
+        }, 500);
     };
 
     const gameLoop = () => {
@@ -41,43 +47,43 @@ export default function Practice($container) {
 
     const movePaddle = () => {
         if (keys['w']) {
-            player1.y = Math.max(player1.y - paddleInfo.speed, 0);
+            player1.y = Math.max(player1.y - paddle.speed, 0);
         }
         if (keys['s']) {
-            player1.y = Math.min(player1.y + paddleInfo.speed, canvas.height - paddleInfo.height);
+            player1.y = Math.min(player1.y + paddle.speed, canvas.height - paddle.height);
         }
         if (keys['ArrowUp']) {
-            player2.y = Math.max(player2.y - paddleInfo.speed, 0);
+            player2.y = Math.max(player2.y - paddle.speed, 0);
         }
         if (keys['ArrowDown']) {
-            player2.y = Math.min(player2.y + paddleInfo.speed, canvas.height - paddleInfo.height);
+            player2.y = Math.min(player2.y + paddle.speed, canvas.height - paddle.height);
         }
     };
 
     const update = () => {
         if (!isBallMoving) return;
         // 공의 위치 업데이트
-        ball.x += ballInfo.velocityX;
-        ball.y += ballInfo.velocityY;
+        ball.x += pong.speed * ball.dirX;
+        ball.y += pong.speed * ball.dirY;
         // 벽 충돌 검사
-        if (ball.y + ballInfo.radius > canvas.height || ball.y - ballInfo.radius < 0) {
-            ballInfo.velocityY = -ballInfo.velocityY; // Y 방향 반전
+        if (ball.y + pong.radius > canvas.height || ball.y - pong.radius < 0) {
+            ball.dirY = -ball.dirY; // Y 방향 반전
         }
         // 공이 왼쪽 또는 오른쪽 끝에 도달했을 때 점수 처리
-        if (ball.x - ballInfo.radius < 0) { // 왼쪽 벽에 충돌
+        if (ball.x - pong.radius < 0) { // 왼쪽 벽에 충돌
             player2.score++;
             resetBall();
-        } else if (ball.x + ballInfo.radius > canvas.width) { // 오른쪽 벽에 충돌
+        } else if (ball.x + pong.radius > canvas.width) { // 오른쪽 벽에 충돌
             player1.score++;
             resetBall();
         }
         // 패들 충돌 검사
         let nearestPlayer = (ball.x < canvas.width / 2) ? player1 : player2;
-        if (ball.x - ballInfo.radius <= nearestPlayer.x + paddleInfo.width
-            && ball.x + ballInfo.radius >= nearestPlayer.x
-            && ball.y - ballInfo.radius <= nearestPlayer.y + paddleInfo.height
-            && ball.y + ballInfo.radius >= nearestPlayer.y) {
-            ballInfo.velocityX = -ballInfo.velocityX; // X 방향 반전
+        if (ball.x - pong.radius <= nearestPlayer.x + paddle.width
+            && ball.x + pong.radius >= nearestPlayer.x
+            && ball.y - pong.radius <= nearestPlayer.y + paddle.height
+            && ball.y + pong.radius >= nearestPlayer.y) {
+            ball.dirX = -ball.dirX; // X 방향 반전
         }
     };
 
@@ -86,8 +92,8 @@ export default function Practice($container) {
         ball.x = canvas.width / 2;
         ball.y = canvas.height / 2;
         // 랜덤한 방향으로 시작 각도 조정
-        ballInfo.velocityX = ballInfo.speed * (Math.random() > 0.5 ? 1 : -1);
-        ballInfo.velocityY = ballInfo.speed * (Math.random() > 0.5 ? 1 : -1);
+        ball.dirX = -ball.dirX;
+        ball.dirY = Math.random() > 0.5 ? 1 : -1;
         setTimeout(() => {
             isBallMoving = true;
         }, 500);
@@ -97,14 +103,14 @@ export default function Practice($container) {
         // 배경 클리어
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         // 플레이어 패들 그리기
-        ctx.fillStyle = paddleInfo.color;
-        ctx.fillRect(player1.x, player1.y, paddleInfo.width, paddleInfo.height);
-        ctx.fillStyle = paddleInfo.color;
-        ctx.fillRect(player2.x, player2.y, paddleInfo.width, paddleInfo.height);
+        ctx.fillStyle = paddle.color;
+        ctx.fillRect(player1.x, player1.y, paddle.width, paddle.height);
+        ctx.fillStyle = paddle.color;
+        ctx.fillRect(player2.x, player2.y, paddle.width, paddle.height);
         // 공 그리기
         ctx.beginPath();
-        ctx.arc(ball.x, ball.y, ballInfo.radius, 0, Math.PI*2, true);
-        ctx.fillStyle = ballInfo.color;
+        ctx.arc(ball.x, ball.y, pong.radius, 0, Math.PI*2, true);
+        ctx.fillStyle = pong.color;
         ctx.fill();
         ctx.closePath();
         // 점수 그리기
