1cb1ed4f86c7c659b3497e463b13a830dbaa8608
jonnwon <bssong35@gmail.com>
2024-03-15 20:24:10 +0900

    feat: freind list 함수 내 self 의 친구리스트 가져오기 구현

--- srcs/BackEnd/project/connect/consumers.py
+++ srcs/BackEnd/project/connect/consumers.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import AnonymousUser
 from channels.generic.websocket import AsyncWebsocketConsumer
 from .models import Client
 from asgiref.sync import sync_to_async
+from channels.db import database_sync_to_async
 
 class ConnectConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -33,7 +34,11 @@ class ConnectConsumer(AsyncWebsocketConsumer):
             self.channel_name
         )
         await self.accept()
-    
+        
+        await self.channel_layer.group_send(
+          self.room_group_name, {"type": "friend_list", "nickname": user}
+        )
+        
     async def disconnect(self, message):
         
         client = await sync_to_async(Client.objects.get)(channel_name=self.channel_name)
@@ -44,3 +49,31 @@ class ConnectConsumer(AsyncWebsocketConsumer):
             self.room_group_name,
             self.channel_name
         )
+    
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        type = text_data_json["type"]
+        nickname = text_data_json["nickname"]
+        
+        if type == 'friend_list':
+            await self.channel_layer.group_send(
+                self.room_group_name, {"type": "friend_list", "nickname": nickname}
+            )
+    
+    async def friend_list(self, event):
+        user = self.scope['user']
+        nickname = event["nickname"]
+        print("User in friend_liste: ", user)
+        print("Nickname: ", nickname)
+        
+        if user == nickname:
+            print("user == nickname")
+            
+            friends = await database_sync_to_async(lambda: list(user.friendlist.all()))()
+           
+            # 친구 목록 출력
+            for friend in friends:
+                print("Friend: ", friend.nickname)
+        else:
+            print("user != nickname") 
+    
