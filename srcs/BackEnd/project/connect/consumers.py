9d44111c9b8906a1ca2d294dbad93c3c7916c7d6
geonwule <geonwlee22@gmail.com>
2024-03-15 20:04:54 +0900

    :hammer: refactor: 웹소켓용 모델 Clinet 생성, consumers 수정
    - models.py에 Client 모델 추가
    - consumers.py에 Connect -> Clinet생성, Disconnect -> Clinet삭제 로직 추가

--- srcs/BackEnd/project/connect/consumers.py
+++ srcs/BackEnd/project/connect/consumers.py
@@ -1,6 +1,8 @@
 import json
 from django.contrib.auth.models import AnonymousUser
 from channels.generic.websocket import AsyncWebsocketConsumer
+from .models import Client
+from asgiref.sync import sync_to_async
 
 class ConnectConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -21,6 +23,10 @@ class ConnectConsumer(AsyncWebsocketConsumer):
             #     self.channel_name
             # )
             # await self.accept()
+            #동기적인 코드를 비동기적으로 실행하기 위해 sync_to_async를 사용 : User, Client, save
+            client = await sync_to_async(Client.objects.create)(channel_name=self.channel_name, user=user)
+            await sync_to_async(client.save)()
+            print("client created, ", client)
             
         await self.channel_layer.group_add(
             self.room_group_name,
@@ -30,6 +36,10 @@ class ConnectConsumer(AsyncWebsocketConsumer):
     
     async def disconnect(self, message):
         
+        client = await sync_to_async(Client.objects.get)(channel_name=self.channel_name)
+        await sync_to_async(client.delete)()
+        print("client deleted")
+            
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
--- srcs/BackEnd/project/connect/models.py
+++ srcs/BackEnd/project/connect/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from user.models import User
 
-# Create your models here.
+class Client(models.Model):
+    channel_name = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='client')
