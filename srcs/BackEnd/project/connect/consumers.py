22277d65b19d89baa9812e599779e16ed01925a6
geonwule <geonwlee22@gmail.com>
2024-03-16 14:57:10 +0900

    :hammer: refactor: 웹소켓 컨슈머 sync_to_async -> database_sync_to_async로 수정

--- srcs/BackEnd/project/connect/consumers.py
+++ srcs/BackEnd/project/connect/consumers.py
@@ -2,7 +2,6 @@ import json
 from django.contrib.auth.models import AnonymousUser
 from channels.generic.websocket import AsyncWebsocketConsumer
 from .models import Client
-from asgiref.sync import sync_to_async
 from channels.db import database_sync_to_async
 
 class ConnectConsumer(AsyncWebsocketConsumer):
@@ -16,8 +15,8 @@ class ConnectConsumer(AsyncWebsocketConsumer):
             # await self.close()
         else:
             print("user is authenticated:", user)
-            client = await sync_to_async(Client.objects.create)(channel_name=self.channel_name, user=user)
-            await sync_to_async(client.save)()
+            client = await database_sync_to_async(Client.objects.create)(channel_name=self.channel_name, user=user)
+            await database_sync_to_async(client.save)()
             print("client created, ", client)
             
             # await self.channel_layer.group_add(
@@ -25,7 +24,7 @@ class ConnectConsumer(AsyncWebsocketConsumer):
             #     self.channel_name
             # )
             # await self.accept()
-            #동기적인 코드를 비동기적으로 실행하기 위해 sync_to_async를 사용 : User, Client, save
+            #동기적인 코드를 비동기적으로 실행하기 위해 database_sync_to_async를 사용 : User, Client, save
             
         await self.channel_layer.group_add(
             self.room_group_name,
@@ -40,8 +39,8 @@ class ConnectConsumer(AsyncWebsocketConsumer):
     async def disconnect(self, message):
         user = self.scope['user']
         
-        client = await sync_to_async(Client.objects.get)(channel_name=self.channel_name)
-        await sync_to_async(client.delete)()
+        client = await database_sync_to_async(Client.objects.get)(channel_name=self.channel_name)
+        await database_sync_to_async(client.delete)()
         print("client deleted")
             
         await self.channel_layer.group_discard(
