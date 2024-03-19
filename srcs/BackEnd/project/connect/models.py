7077c4df78f930d2f03b05c4fdbcde41783f0a63
Kannu <geonwlee22@gmail.com>
2024-03-20 01:33:43 +0900

    :sparkles: feat: 초대 대기열 모델 생성

--- srcs/BackEnd/project/connect/models.py
+++ srcs/BackEnd/project/connect/models.py
@@ -4,3 +4,7 @@ from user.models import User
 class Client(models.Model):
     channel_name = models.CharField(max_length=100)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='client')
+    
+class InvitationQueue(models.Model):
+    sender = models.CharField(max_length=30)
+    receiver = models.CharField(max_length=30)
