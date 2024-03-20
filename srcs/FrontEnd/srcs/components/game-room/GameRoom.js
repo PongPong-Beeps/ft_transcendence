febbd99b42ac5414f69c372a9c4563a9a3e845cc
noeyiz <lamoureclatant@gmail.com>
2024-03-20 16:37:48 +0900

    ✨ feat: GameRoom 컴포넌트 생성

---
+++ srcs/FrontEnd/srcs/components/game-room/GameRoom.js
@@ -0,0 +1,48 @@
+import PlayerInfo from "./PlayerInfo.js";
+import ExitConfirmation from "../../pages/ExitConfirmation.js";
+import {importCss} from "../../utils/importCss.js";
+import ErrorPage from "../../pages/ErrorPage.js";
+
+/**
+ * @param {HTMLElement} $container
+ * @param { Object } data
+ */
+export default function VsRoom($container, data) {
+    const init = () => {
+        $container.querySelectorAll('.invite-btn').forEach(button => {
+            button.style.display = 'block';
+        });
+    }
+
+    const render = () => {
+        $container.querySelector('#main').innerHTML = `
+            <div class="game-room-container">
+                <div class="game-room-title-container">
+                    <button class="game-room-back-btn non-outline-btn"><</button>
+                    <div class="game-room-title">게임방</div>
+                    <div class="game-room-difficulty">타입 : ${data.type === "one-to-one" ? "1:1" : "토너먼트"} 모드 : ${data.mode === "easy" ? "쉬움" : "어려움"}</div>
+                </div>
+                <div class="game-room-player-container">
+                    ${PlayerInfo()}
+                    ${PlayerInfo()}
+                </div>
+            </div>
+        `;
+    }
+
+    const setupEventListener = () => {
+        $container.querySelector('.game-room-back-btn').addEventListener('click', () => {
+            new ExitConfirmation($container);
+        });
+    }
+
+    if (data === undefined) {
+        new ErrorPage($container);
+        return;
+    }
+
+    importCss("assets/css/game-room.css")
+    init();
+    render();
+    setupEventListener();
+}
