7d4838f55f7fb96fb35dc432041720f71858594a
noeyiz <lamoureclatant@gmail.com>
2024-03-22 17:46:36 +0900

    🔥 remove: 사용하지 않는 파일 제거

--- srcs/FrontEnd/srcs/constants/routeInfo.js
+++ srcs/FrontEnd/srcs/constants/routeInfo.js
@@ -1,10 +1,8 @@
 import Chat from "../components/Chat.js";
 import DisplayBoard from "../components/DisplayBoard.js";
 import GameSettings from "../components/game-settings/GameSettings.js";
-import TournamentRoom from "../components/game-room/TournamentRoom.js";
 import UserList from "../components/user-list/UserList.js";
 import LoginPage from "../pages/LoginPage.js";
-import VsRoom from "../components/game-room/VsRoom.js";
 import VsSchedule from "../pages/vs-schedule/VsSchedule.js";
 import AuthPage from "../pages/AuthPage.js";
 import Practice from "../components/game/practice.js";
@@ -16,8 +14,6 @@ export const routes = [
     {path: /^\/lobby$/, layout: "grid", components: { menu: UserList, main: GameSettings, footer: Chat }},
     {path: /^\/practice$/, layout: "grid", components: { menu: UserList, main: Practice, footer: Chat }},
     {path: /^\/game-room$/, layout: "grid", components: { menu: UserList, main: GameRoom, footer: Chat }},
-    {path: /^\/tournament-room$/, layout: "grid", components: { menu: UserList, main: TournamentRoom, footer: Chat }},
-    {path: /^\/vs-room$/, layout: "grid", components: { menu: UserList, main: VsRoom, footer: Chat }},
-    {path: /^\/vs-game$/, layout: "grid", components: { menu: UserList, main: VsRoom, footer: DisplayBoard }},
+    {path: /^\/vs-game$/, layout: "grid", components: { menu: UserList, main: GameRoom, footer: DisplayBoard }},
     {path: /^\/vs-schedule$/, layout: "full", page: VsSchedule},
 ]
