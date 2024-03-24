8ee3420bd3d072df7cdff90d4c3aa996c9775df6
jonnwon <bssong35@gmail.com>
2024-03-25 06:59:10 +0900

    :sparkles: feat: Round 모델 직렬화 함수 추가

---
+++ srcs/BackEnd/project/game/serializer.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Round
+
+class RoundSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Round
+        fields = '__all__'
