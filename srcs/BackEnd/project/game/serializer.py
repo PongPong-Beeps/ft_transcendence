3ba65069ba5eaa5890664aa75a5b7efe7b9b0e57
jonnwon <bssong35@gmail.com>
2024-03-25 19:47:00 +0900

    :fire: remove: game앱 내 serializer.py 삭제

--- srcs/BackEnd/project/game/serializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Round
-
-class RoundSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Round
-        fields = '__all__'
