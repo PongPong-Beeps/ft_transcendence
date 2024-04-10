ba40512d0bbda8162121a8692973dd6121098564
jonnwon <bssong35@gmail.com>
2024-04-10 16:28:22 +0900

    :hammer: refactor: settings 내 SECRET_KEY 환경변수로 설정

--- srcs/BackEnd/project/project/settings.py
+++ srcs/BackEnd/project/project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k_qnco^zlm0)2kkewpq1@v+qpv^f&c#0e#&0!_gocz6j(d&$j('
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
